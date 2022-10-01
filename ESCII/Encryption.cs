7f5ac80eddf5b2f4a104a53d320b985b5e8a33fd
zonetecde <zonetecde@gmail.com>
2022-10-01 22:48:16 +0200

    Ajoutez des fichiers projet.

---
+++ ESCII/Encryption.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESCII
+{
+    internal static class Encryption
+    {
+        /// <summary>
+        /// Décrypte l'image
+        /// </summary>
+        /// <param name="bitmap_img"></param>
+        /// <returns></returns>
+        internal static string Decryption(Bitmap bitmap_img, string key)
+        {
+            string code = String.Empty;
+            int compteur = 0;
+
+            // si clé on prend ses infos
+            int minX = 1;
+            int minY = 1;
+            int pasX = 2;
+            int pasY = 2;
+            if (!String.IsNullOrEmpty(key))
+            {
+                // min X : 5, 3, 8, 9, 0
+                minX = Convert.ToInt32(Math.Abs((int)key[5] + (int)key[3] - (int)key[8] * (int)key[9] - (int)key[0]).ToString().Remove(2));
+
+                // min Y : 1, 15, 19, 4
+                minY = Convert.ToInt32(Math.Abs((int)key[1] - (int)key[15] - (int)key[19] * (int)key[4]).ToString().Remove(2));
+
+                // pas X : 2, 14, 17,  2
+                pasX = Convert.ToInt32(Math.Abs((int)key[2] - (int)key[14] * (int)key[17] * (int)key[2]).ToString().Remove(2));
+
+                // pas Y : 6, 10, 7,  11
+                pasX = Convert.ToInt32(Math.Abs((int)key[6] * (int)key[10] + (int)key[7] - (int)key[11]).ToString().Remove(2));
+
+            }
+
+            for (int x = minX; x < bitmap_img.Width - 1; x+= pasX)
+            {
+                MainWindow.bgWorker_decryptage.ReportProgress(x);
+
+                for (int y = minY; y < bitmap_img.Height - 1; y+= pasY)
+                {
+                    // ré init max char
+                    if (compteur == 253)
+                    {
+                        compteur = 0;
+                    }
+
+                    // pixel haut bas gauche droite de la même couleur
+                    if (bitmap_img.GetPixel(x, y) == bitmap_img.GetPixel(x - 1, y) &&
+                       bitmap_img.GetPixel(x, y) == bitmap_img.GetPixel(x + 1, y) &&
+                       bitmap_img.GetPixel(x, y) == bitmap_img.GetPixel(x, y + 1) &&
+                       bitmap_img.GetPixel(x, y) == bitmap_img.GetPixel(x, y - 1) 
+                       )
+                    {
+                        code += (char)compteur;
+                    }
+
+                    compteur++;
+
+                }
+            }
+
+            MainWindow.SecretMessage = code;
+            return code;
+        }
+
+        /// <summary>
+        /// Encrypte l'image
+        /// </summary>
+        /// <param name="bitmap_img">L'image a coder</param>
+        /// <param name="code">Le message secret</param>
+        internal static Bitmap Encrypt(Bitmap bitmap_img, string code, string key, bool report = true)
+        {
+            try
+            {
+                // variable temporaire pour compter
+                int compteur = 0;
+
+                // les pixels où le message sera encodé
+                List<int> pixelsToReplace = new List<int>();
+                // récupération des pixels où le message sera encodé
+                // pour chaque lettre du code secret
+                code.ToCharArray().ToList().ForEach(letter =>
+                {
+                    // on récupère sa valeur en nombre char
+                    int letterCode = letter;
+
+                    // le convertit en pixel a remplacé
+                    pixelsToReplace.Add(compteur + letterCode);
+
+                    // 253 = max char ASCII
+                    compteur += 253;
+                });
+
+                // ré init le compteur
+                compteur = 0;
+                int letteurDone = 0;
+
+                // si clé on prend ses infos
+                int minX = 1;
+                int minY = 1;
+                int pasX = 2;
+                int pasY = 2;
+
+                if(!String.IsNullOrEmpty(key))
+                {
+                    // min X : 5, 3, 8, 9, 0
+                    minX = Convert.ToInt32(Math.Abs((int)key[5] + (int)key[3] - (int)key[8] * (int)key[9] - (int)key[0]).ToString().Remove(2));
+                    
+                    // min Y : 1, 15, 19, 4
+                    minY = Convert.ToInt32(Math.Abs((int)key[1] - (int)key[15] - (int)key[19] * (int)key[4]).ToString().Remove(2));
+
+                    // pas X : 2, 14, 17,  2
+                    pasX = Convert.ToInt32(Math.Abs((int)key[2] - (int)key[14] * (int)key[17] * (int)key[2]).ToString().Remove(2));
+                                        
+                    // pas Y : 6, 10, 7,  11
+                    pasX = Convert.ToInt32(Math.Abs((int)key[6]* (int)key[10] + (int)key[7] - (int)key[11]).ToString().Remove(2));
+
+                }
+
+                for (int x = minX; x < bitmap_img.Width - 1; x += pasX)
+                {
+                    if(report)
+                    MainWindow.bgWorker_cryptage.ReportProgress(x);
+
+                    for (int y = minY; y < bitmap_img.Height - 1; y += pasY)
+                    {
+                        // Si 5 pixels sont de la même couleur exactement et que le pixel ne fait pas partie de la liste secrète
+                        if (bitmap_img.GetPixel(x, y) == bitmap_img.GetPixel(x - 1, y) &&
+                            bitmap_img.GetPixel(x, y) == bitmap_img.GetPixel(x + 1, y) &&
+                            bitmap_img.GetPixel(x, y) == bitmap_img.GetPixel(x, y + 1) &&
+                            bitmap_img.GetPixel(x, y) == bitmap_img.GetPixel(x, y - 1)
+                            && !pixelsToReplace.Contains(compteur))
+                        {
+                            // on modifie alors le pixel pour que il n'y est plus que 4 pixels de la même couleur
+                            if (bitmap_img.GetPixel(x, y).R < 255)
+                            {
+                                bitmap_img.SetPixel(x, y, Color.FromArgb(255,
+                                    bitmap_img.GetPixel(x, y).R + 1, bitmap_img.GetPixel(x, y).G, bitmap_img.GetPixel(x, y).B));
+                            }
+                            else
+                            {
+                                bitmap_img.SetPixel(x, y, Color.FromArgb(255,
+                                    bitmap_img.GetPixel(x, y).R - 1, bitmap_img.GetPixel(x, y).G, bitmap_img.GetPixel(x, y).B));
+                            }
+                        }
+                        else if (pixelsToReplace.Contains(compteur)) // si le pixel fait partie du code secret
+                        {
+                            // les pixels haut bas gauche & droite doivent être de la même couleur
+                            bitmap_img.SetPixel(x - 1, y, bitmap_img.GetPixel(x, y));
+                            bitmap_img.SetPixel(x + 1, y, bitmap_img.GetPixel(x, y));
+                            bitmap_img.SetPixel(x, y - 1, bitmap_img.GetPixel(x, y));
+                            bitmap_img.SetPixel(x, y + 1, bitmap_img.GetPixel(x, y));
+                        }
+
+                        if (pixelsToReplace.Contains(compteur))
+                            letteurDone++;
+
+                        compteur++;
+                    }
+                }
+
+                if (letteurDone == pixelsToReplace.Count)
+                {
+                    MainWindow.output = bitmap_img;
+                    return bitmap_img;
+                }
+                else
+                {
+                    MainWindow.output = null;
+                    return null; // image trop petite
+                }
+            }
+            catch
+            {
+                MainWindow.output = null;
+                return null;
+            }
+        }
+
+        public class DirectBitmap : IDisposable
+        {
+            public Bitmap Bitmap { get; private set; }
+            public Int32[] Bits { get; private set; }
+            public bool Disposed { get; private set; }
+            public int Height { get; private set; }
+            public int Width { get; private set; }
+
+            protected GCHandle BitsHandle { get; private set; }
+
+            public DirectBitmap(int width, int height)
+            {
+                Width = width;
+                Height = height;
+                Bits = new Int32[width * height];
+                BitsHandle = GCHandle.Alloc(Bits, GCHandleType.Pinned);
+                Bitmap = new Bitmap(width, height, width * 4, PixelFormat.Format32bppPArgb, BitsHandle.AddrOfPinnedObject());
+            }
+
+            public void SetPixel(int x, int y, Color colour)
+            {
+                int index = x + (y * Width);
+                int col = colour.ToArgb();
+
+                Bits[index] = col;
+            }
+
+            public Color GetPixel(int x, int y)
+            {
+                int index = x + (y * Width);
+                int col = Bits[index];
+                Color result = Color.FromArgb(col);
+
+                return result;
+            }
+
+            public void Dispose()
+            {
+                if (Disposed) return;
+                Disposed = true;
+                Bitmap.Dispose();
+                BitsHandle.Free();
+            }
+        }
+    }
+}
---
+++ ESCII/MainWindow.xaml.cs
@@ -0,0 +1,369 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Brushes = System.Windows.Media.Brushes;
+using Color = System.Drawing.Color;
+using Image = System.Windows.Controls.Image;
+using PixelFormat = System.Drawing.Imaging.PixelFormat;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System.ComponentModel;
+
+namespace ESCII
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random rdn = new Random();
+
+        internal static BackgroundWorker bgWorker_decryptage = new BackgroundWorker();
+        internal static BackgroundWorker bgWorker_cryptage = new BackgroundWorker();
+
+        internal static Bitmap output;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            bgWorker_decryptage.WorkerReportsProgress = true;
+            bgWorker_decryptage.ProgressChanged += new ProgressChangedEventHandler(bgWorker_decrypt_progressChanged);
+            bgWorker_decryptage.DoWork += new DoWorkEventHandler(bgWorker_decrypt_doWork);
+           
+        }
+
+        private void bgWorker_crypt_progressChanged(object? sender, ProgressChangedEventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                progressBar_cryptage.Value = e.ProgressPercentage;
+
+                double p = Math.Round(((e.ProgressPercentage / progressBar_cryptage.Maximum) * 100), 2);
+                button_crypt.Content = p.ToString().PadLeft(3) + "%";
+            });
+        }
+
+        private void bgWorker_crypt_doWork(object? sender, DoWorkEventArgs e)
+        {
+            string text= "";
+            bool? isChecked = false;
+            string key = "";
+
+
+            Bitmap r = (e.Argument as Bitmap);
+
+            Dispatcher.Invoke(() =>
+            {
+                text = new TextRange(richTextBox_code.Document.ContentStart, // texte
+                richTextBox_code.Document.ContentEnd).Text;
+
+                isChecked = cb_useKey.IsChecked;
+                key = txtBox_Key.Text;
+            });
+
+
+
+            Encryption.Encrypt(r,
+                text, isChecked == true ? key : String.Empty);
+
+        }
+
+        private void Button_Encrypter_Click(object sender, RoutedEventArgs e)
+        {
+            string text = new TextRange(richTextBox_code.Document.ContentStart, // texte
+                richTextBox_code.Document.ContentEnd).Text;
+
+            if (text != "\r\n") // = vide
+                foreach (Border border in wrapPanel_images.Children)
+                {
+                    if (border.BorderThickness.Left == 2)
+                    {
+                        var b = ConvertToBitmap(
+                                (BitmapSource)(border.Child as Image).Source);
+                    
+                        if (cb_useKey.IsChecked == true)
+                            if (txtBox_Key.Text.Length < 20)
+                            {
+                                MessageBox.Show("La clé doit faire 20 caractères.", "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
+                                return;
+                            }
+                        bgWorker_cryptage = new BackgroundWorker();
+                        bgWorker_cryptage.WorkerReportsProgress = true;
+                        bgWorker_cryptage.ProgressChanged += new ProgressChangedEventHandler(bgWorker_crypt_progressChanged);
+                        bgWorker_cryptage.DoWork += new DoWorkEventHandler(bgWorker_crypt_doWork);
+
+                        progressBar_cryptage.Maximum = b.Width;
+                        bgWorker_cryptage.RunWorkerAsync(b);
+
+                        bgWorker_cryptage.RunWorkerCompleted += new RunWorkerCompletedEventHandler((sender, e) =>
+                        {
+                            progressBar_cryptage.Value = 0;
+                            button_crypt.Content = "Encrypter";
+
+                            if (output != null)
+                            {
+                                var dialog = new CommonOpenFileDialog();
+                                dialog.IsFolderPicker = true;
+                                CommonFileDialogResult result = dialog.ShowDialog();
+
+                                if (result == CommonFileDialogResult.Ok)
+                                {
+                                    output.Save(dialog.FileName + @"\output " + DateTime.Now.ToString("dd_MM_yyyy HH_mm_ss") + ".png", ImageFormat.Png);
+                                    output.Dispose();
+                                }
+                            }
+                            else
+                            {
+                                // réessaie avec une autre clé
+                                if (cb_useKey.IsChecked == true)
+                                {
+                                    for (int i = 0; i < 20; i++)
+                                    {
+                                        string newK = GénérerClé();
+                                        Bitmap btm = Encryption.Encrypt(b,
+                                            text, newK, false);
+
+                                        if (btm != null)
+                                        {
+                                            if (MessageBox.Show("La clé actuelle ne permet pas d'encoder cette image.\nVoulez-vous utiliser la clé \"" + newK + "\" ? \nElle sera enregistrer dans le presse papier.", "Information", MessageBoxButton.YesNo, MessageBoxImage.Information) == MessageBoxResult.Yes)
+                                            {
+                                                Clipboard.SetText(newK);
+
+                                                var dialog = new CommonOpenFileDialog();
+                                                dialog.IsFolderPicker = true;
+                                                CommonFileDialogResult result = dialog.ShowDialog();
+
+                                                if (result == CommonFileDialogResult.Ok)
+                                                {
+                                                    btm.Save(dialog.FileName + @"\output " + DateTime.Now.ToString("dd_MM_yyyy HH_mm_ss") + ".png", ImageFormat.Png);
+                                                    btm.Dispose();
+                                                }
+                                            }
+
+                                            return;
+                                        }
+
+                                    }
+                                }
+                                MessageBox.Show("L'image est trop petite pour contenir votre message secret. Veuillez en choisir une autre.", "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
+                            }
+                        });
+
+
+                       
+                    
+                    }
+                }
+            else
+                MessageBox.Show("Veuillez écrire la chose à encrypter dans l'image.", "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
+            //MessageBox.Show(Encryption.Decryption(new Bitmap("output474.png")));
+
+        }
+
+        private void Import_Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            dlg.DefaultExt = "Image Files|*.jpg;*.jpeg;*.png;*.bmp;";
+            dlg.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp;";
+
+            Nullable<bool> result = dlg.ShowDialog();
+
+            if (result == true)
+            {
+                string filename = dlg.FileName;
+
+                Border b = new Border();
+                var img = new System.Windows.Controls.Image()
+                {
+                    Source = new BitmapImage(new Uri(filename)),
+                    Margin = new Thickness(10),
+                    Cursor = Cursors.Hand,
+                };
+                b.Child = img;
+
+                // sélectionne
+                b.BorderBrush = Brushes.Yellow;
+                b.BorderThickness = new Thickness(2);
+
+                b.MouseDown += new MouseButtonEventHandler(ImageSelected);
+
+                // enlève effet sélectionner des autres
+                foreach (Border border in wrapPanel_images.Children)              
+                    border.BorderThickness = new Thickness(0);
+                
+
+                wrapPanel_images.Children.Insert(1, b);
+            }
+        }
+
+        private void ImageSelected(object sender, MouseButtonEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed)
+                foreach(Border border in wrapPanel_images.Children)
+                {
+                    border.BorderThickness = new Thickness(0);
+
+                    if (border == (sender as Border))
+                        border.BorderThickness = new Thickness(2);
+                }
+        }
+
+        private Bitmap ConvertToBitmap(BitmapSource bitmapSource)
+        {
+            var width = bitmapSource.PixelWidth;
+            var height = bitmapSource.PixelHeight;
+            var stride = width * ((bitmapSource.Format.BitsPerPixel + 7) / 8);
+            var memoryBlockPointer = Marshal.AllocHGlobal(height * stride);
+            bitmapSource.CopyPixels(new Int32Rect(0, 0, width, height), memoryBlockPointer, height * stride, stride);
+            var bitmap = new Bitmap(width, height, stride, PixelFormat.Format32bppPArgb, memoryBlockPointer);
+            return bitmap;
+        }
+
+        private void Button_GénérerClé_Click(object sender, RoutedEventArgs e)
+        {
+            string password = GénérerClé();
+            txtBox_Key.Text = password;
+        }
+
+        private string GénérerClé()
+        {
+            string valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890&[]}{)(@*$%/\\+-<>&[]}{)(@*$%/\\+-<>";
+            string password = string.Empty;
+            for (int i = 0; i < 20; i++)
+            {
+                password += valid[rdn.Next(0, valid.Length)];
+            }
+
+            return password;
+        }
+
+        private void txtBox_Key_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                grid_usekey.Visibility = Visibility.Hidden;
+        }
+
+        private void txtBox_Key_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            label_nbChar.Content = txtBox_Key.Text.Length.ToString().PadLeft(2) + "/20";
+        }
+
+        private void grid_usekey_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                grid_usekey.Visibility = Visibility.Hidden;
+        }
+
+        private void Button_CloseGridKey_Click(object sender, RoutedEventArgs e)
+        {
+            grid_usekey.Visibility = Visibility.Hidden;
+        }
+
+        private void cb_useKey_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Button_ChoisirImageDécrypter_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            dlg.DefaultExt = "Image Files|*.jpg;*.jpeg;*.png;*.bmp;";
+            dlg.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp;";
+
+            Nullable<bool> result = dlg.ShowDialog();
+
+            if (result == true)
+            {
+                string filename = dlg.FileName;
+                (sender as Button).Content = filename;
+            }
+        }
+
+        internal static string SecretMessage;
+
+        private void Button_Décrypter_Click(object sender, RoutedEventArgs e)
+        {
+            richTextBox_output.Document.Blocks.Clear();
+
+            if(!button_decrypter.Content.ToString().Contains("%"))
+            if(!button_imageFile.Content.ToString().Contains("Choisir une image") )
+            {
+                if (txtBox_decryptKey.Text.Length == 0 || txtBox_decryptKey.Text.Length == 20)
+                {
+                    bgWorker_decryptage.RunWorkerAsync(new Bitmap(button_imageFile.Content.ToString()));
+
+                    bgWorker_decryptage.RunWorkerCompleted += new RunWorkerCompletedEventHandler((sender, e) =>
+                    {
+                        progressBar_decryptage.Value = 0;
+                        button_decrypter.Content = "Décrypter";
+                        richTextBox_output.AppendText(SecretMessage);
+                    });
+                }
+                else
+                    MessageBox.Show("Clé invalide.", "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+                MessageBox.Show("Veuillez choisir une image", "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        /// <summary>
+        /// Décryptage worker
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        private void bgWorker_decrypt_doWork(object? sender, DoWorkEventArgs e)
+        {
+            string key = "";
+            Dispatcher.Invoke(() =>
+            {
+                key = txtBox_decryptKey.Text;
+                progressBar_decryptage.Maximum = (e.Argument as Bitmap).Width - 2;
+            });
+            Encryption.Decryption(e.Argument as Bitmap, key);
+            
+        }
+
+        /// <summary>
+        /// Décryptage progressChanged
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        private void bgWorker_decrypt_progressChanged(object? sender, ProgressChangedEventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                progressBar_decryptage.Value = e.ProgressPercentage;
+
+                double p = Math.Round(((e.ProgressPercentage / progressBar_decryptage.Maximum) * 100), 2);
+                button_decrypter.Content = p.ToString().PadLeft(3) + "%";
+            });
+        }
+
+        private void cb_useKey_Checked(object sender, RoutedEventArgs e)
+        {
+            if (cb_useKey.IsChecked == true)
+            {
+                grid_usekey.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
